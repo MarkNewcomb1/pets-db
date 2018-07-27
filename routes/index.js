14c49d52a49d232b6ec04320d518891df3ad7446
MarkNewcomb1 <wlmmn@hotmail.com>
2018-07-27 13:43:37 -0600

    changes from breakout

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db/knex.js')
+var knex = require('../db/knex.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("GET GOT!")
+  knex.select('').from('person').where('name', 'Mark')
+  .then(function(data){
+    res.json(data);
+  })
 })
 
 module.exports = router;
--- seeds/01_person.js
+++ seeds/01_person.js
@@ -7,6 +7,7 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('person').insert({id: 1, name: 'Peter'}),
     knex('person').insert({id: 2, name: 'James'}),
-    knex('person').insert({id: 3, name: 'Brooks'})
+    knex('person').insert({id: 3, name: 'Brooks'}),
+    knex('person').insert({id: 4, name: 'Mark'})
   )
 }
